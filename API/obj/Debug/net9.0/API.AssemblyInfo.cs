a9d60249e13681568cec0dcca81f1bfc400a9efe
KHOLUD AHMED <kholudahmed666@gmail.com>
2025-10-25 13:33:13 +0300

    RevokeAccess

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfacf79cef99f03bf72a035a5a84d4710e8348a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+311cb254e7aab612be4437803cee76c24ddd3172")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

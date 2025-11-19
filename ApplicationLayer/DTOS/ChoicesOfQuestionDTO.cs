974a9b6928f86bd5b2e79c98e74519254b2d26d7
KHOLUD AHMED <kholudahmed666@gmail.com>
2025-11-19 13:51:44 +0200

    solve question bugs

--- ApplicationLayer/DTOS/ChoicesOfQuestionDTO.cs
+++ ApplicationLayer/DTOS/ChoicesOfQuestionDTO.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Application.DTOS
 {
-    public class CreateQuestionChoicesDTO
+    public class ChoicesOfQuestionDTO
     {
+        public int? Id { get; set; }
+        [Required(ErrorMessage = "Choice فext is required")]
         public string ChoiceText { get; set; } = string.Empty;
         public bool IsCorrect { get; set; }
+        [Required(ErrorMessage = "Question id  is required")]
         public int QuestionId { get; set; }
     }
 }
